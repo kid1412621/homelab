4fd625f8d8650f249ed93bcfd242aafa11c8324d
alex <kid1412621@gmail.com>
2025-10-22 00:11:26 +0800

    feat: update k3s script for post install

--- k3s.sh
+++ k3s.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -eou pipefail
 
+
+if(command -v "k3s" >/dev/null 2>&1); then
+  exit 0
+fi
+
 curl -sfL https://get.k3s.io | sh -
+
+KUBECONFIG=~/.kube/config
+mkdir ~/.kube 2> /dev/null
+sudo k3s kubectl config view --raw > "$KUBECONFIG"
+chmod 600 "$KUBECONFIG"
